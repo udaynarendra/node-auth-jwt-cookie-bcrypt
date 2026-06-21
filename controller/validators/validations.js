827b5d34986cd365c9b7e960d9f11e15e304b3cd
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-06-21 12:39:13 +0530

    adding validators folder and adding validations.js

---
+++ controller/validators/validations.js
@@ -0,0 +1,38 @@
+import Joi from 'joi';
+
+const registerValidation=Joi.object({
+    name:Joi.string()
+    .trim()
+    .lowercase()
+    .min(3)
+    .max(20)
+    .required(),
+    email:Joi.string()
+    .trim()
+    .lowercase()
+    .email()
+    .required(),
+    password:Joi.string()
+    .trim()
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+    .required()
+    .message({
+    "string.pattern.base":"password must contain uppercase,lowercase,number and special character"
+}) 
+});
+
+const loginValidation=Joi.object({
+email:Joi.string()
+.lowercase()
+.email()
+.required(),
+password:Joi.string()
+.trim()
+.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+.required()
+.message({
+    "string.pattern.base":"password must contain uppercase,lowercase,number and special character"
+}) 
+});
+
+export {loginValidation,registerValidation};
