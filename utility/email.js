d8503c73b1dd166088138cff3571c75dca6b8047
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-06-23 21:08:36 +0530

    adding email.js file

---
+++ utility/email.js
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import nodemailer from 'nodemailer';
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:process.env.USER_EMAIL,
+        auth:process.env.APP_PASSWORD
+    }
+});
+
+const sendVerificationEmail=async(email,token)=>{
+    const verificationUrl=`${process.env.CLIENT_URL}/verify-email?=${token}`;
+    await transporter.sendMail({
+        from:process.env.USER_EMAIL,
+        to:email,
+        subject:'verify your email',
+        html:`
+        <h2>Email Verification</h2>
+        <p>Click the button below to verify your account</p>
+        <a href=${verificationUrl}>Verify Email</a>
+        <p>This link will be expire in 24 hours</p>
+        `
+    });
+};
+
+const sendPasswordResetEmail=async(email,token)=>{
+    const resetUrl=`${process.env.CLIENT_URL}reset-password?token=${token}`;
+    await transporter.sendMail({
+        from:process.env.USER_EMAIL,
+        to:email,
+        subject:'password Reset ',
+        html:`<h2>Email Verification</h2>
+        <p>Click the button below to reset your password</p>
+        <a href=${resetUrl}>Reset Password</a>
+        <p>This link will be expire in 24 hours</p>`
+    })
+}
+
+export {sendPasswordResetEmail,sendVerificationEmail};
