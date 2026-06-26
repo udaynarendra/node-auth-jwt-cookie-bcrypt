a395a4402464194864ea0d85020b36eef65d87d5
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-06-26 21:19:16 +0530

    imporove the error handler

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -4,44 +4,44 @@ import { comparePassword } from '../utility/hashedPassword.js';
 import User from '../models/users.models.js'
 import { generateAccessToken, generateRefreshToken } from '../utility/tokens.js';
 import RefreshToken from '../models/refreshToken.models.js';
+import ApiError from '../utility/apiError.js';
+import asyncHandler from '../utility/asyncHandler.js';
 
 
-const login = async (req, res) => {
-    try {
-        const validateData = await loginValidation.validateAsync(req.body);
-        const user = await User.findOne({ email: validateData.email });
-        if (!user) {
-            return res.status(404).json(apiResponse(false, 'user not found'));
-        }
-        if (user.isVerified === false) {
-
-            return res.status(400).json(apiResponse(false, 'User Email not verified'))
-        }
-        const isMatch = await comparePassword(validateData.password, user.password);
-        if (!isMatch) {
-            return res.status(400).json(apiResponse(false, 'Invalid Creditionals'));
-        }
-
-        const accessToken = generateAccessToken(user._id, user.email, user.role);
-        const refreshToken = generateRefreshToken(user._id);
-        const refreshTokenData = await RefreshToken.create({
-            userId: user._id,
-            token: refreshToken,
-            isRevoked: false,
-            expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
-        });
-        res.cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        });
-
-        return res.status(200).json(apiResponse(true, 'login successfully', { accessToken }));
-    } catch (error) {
-        console.log(error.message)
-        return res.status(500).json(apiResponse(false, 'Internal Server Error'));
+const login = asyncHandler(async (req, res) => {
+    const validateData = await loginValidation.validateAsync(req.body);
+    const user = await User.findOne({ email: validateData.email });
+    if (!user) {
+        throw new ApiError(404, 'user not found');
     }
+    if (user.isVerified === false) {
+
+        throw new ApiError(400, 'User Email not verified');
+    }
+    const isMatch = await comparePassword(validateData.password, user.password);
+    if (!isMatch) {
+
+        throw new ApiError(401, 'Invalid Credentials');
+    }
+
+    const accessToken = generateAccessToken(user._id, user.email, user.role);
+    const refreshToken = generateRefreshToken(user._id);
+    const refreshTokenData = await RefreshToken.create({
+        userId: user._id,
+        token: refreshToken,
+        isRevoked: false,
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+    });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    return res.status(200).json(apiResponse(true, 'login successfully', { accessToken }));
+
 }
+)
 
 export default login;
--- src/controller/refreshToken.controller.js
+++ src/controller/refreshToken.controller.js
@@ -3,28 +3,25 @@ import apiResponse from '../utility/apiResponse.js';
 import User from '../models/users.models.js'
 import { generateAccessToken } from '../utility/tokens.js';
 import RefreshToken from '../models/refreshToken.models.js';
+import asyncHandler from '../utility/asyncHandler.js';
+import ApiError from '../utility/apiError.js';
 
 
-const refreshToken = async (req, res) => {
-    try {
-        
-        const validateData = await refreshTokenValidation.validateAsync(req.cookies.refreshToken);
-        
-        const refreshtoken = await RefreshToken.findOne({ token: validateData.refreshToken }).populate('userId');
-   
-        if (!refreshtoken) {
-            res.status(400).json(apiResponse(false, 'refresh token  not found'));
-        }
-        if(refreshToken.isRevoked){
-            return res.status(401).json(apiResponse(false,'Refresh token has been revoked'));
-        }
-        
-        const user = refreshToken.userId;
-        const newAccessToken = generateAccessToken(user._id, user.email, user.role);
-        return res.status(200).json(apiResponse(true, 'accessToken created successfully', { newAccessToken }));
+const refreshToken = asyncHandler(async (req, res) => {
+
+    const validateData = await refreshTokenValidation.validateAsync(req.cookies.refreshToken);
+
+    const refreshtoken = await RefreshToken.findOne({ token: validateData.refreshToken }).populate('userId');
+
+    if (!refreshtoken) {
+        throw new ApiError(400, 'Refresh Token Not Found');
     }
-    catch (error) {
-        return res.status(500).json(apiResponse(false, 'Internal Server Error'));
+    if (refreshToken.isRevoked) {
+        throw new ApiError(401, 'Refresh token has been revoked')
     }
-}
+
+    const user = refreshToken.userId;
+    const newAccessToken = generateAccessToken(user._id, user.email, user.role);
+    return res.status(200).json(apiResponse(true, 'accessToken created successfully', { newAccessToken }));
+})
 export default refreshToken;
