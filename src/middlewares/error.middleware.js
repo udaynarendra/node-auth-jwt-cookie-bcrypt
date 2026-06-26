aa65782e598c29892d50ada28ae5e1f02a9cb3c3
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-06-26 15:31:20 +0530

    edit some changes

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,4 +1,4 @@
-import ApiError from "./src/utility/apiError.js";
+import ApiError from "../utility/apiError.js";
 const errorHandler = (err, req, res, next) => {
     let error = err;
     if (!(error instanceof ApiError)) {
