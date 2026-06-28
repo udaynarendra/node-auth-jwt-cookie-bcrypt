acc9b34c0484295c0e290a17a78b965a778f6cf5
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-06-28 11:11:54 +0530

    add forgotPassword route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,13 @@ import login from '../controller/login.controller.js';
 import verifyEmail from '../controller/verifyEmail.controller.js';
 import refreshToken from '../controller/refreshToken.controller.js';
 import logOut from '../controller/logout.controller.js';
+import forgotPassword from '../controller/forgotPassword.controller.js';
 const router = express.Router();
 router.post('/register',register);
 router.post('/login',login);
 router.get('/verifyEmail',verifyEmail);
 router.get('/profile',verify,profile);
 router.post('/refreshToken',refreshToken);
-router.post('/logout',logOut)
+router.post('/logout',logOut);
+router.post('/forgotPassword',forgotPassword)
 export default router;
